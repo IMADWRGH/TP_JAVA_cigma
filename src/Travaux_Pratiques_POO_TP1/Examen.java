b2b8249f8fec68c631e984925e301ccf67fcd46f
Imad Asserne <imad.asserne@gmail.com>
2022-12-30 21:33:03 +0100

    first commit

---
+++ src/Travaux_Pratiques_POO_TP1/Examen.java
@@ -0,0 +1,31 @@
+/////////////////////Séance 2 de Java: Exercie 2 ///////////////////
+
+package Travaux_Pratiques_POO_TP1;
+
+public class Examen {
+    private  int code;
+    private  String duree;
+    private String date;
+
+    public void Afficher()
+    {
+        System.out.println("Code "+code+" Duree :"+duree+" Date  :"+date);
+    }
+    public Examen() {
+        this.code = 0;
+        this.duree = "1H30";
+        this.date = "00/00/0000";
+    }
+    public Examen(int code, String duree, String date) {
+        this.code = code;
+        this.duree = duree;
+        this.date = date;
+    }
+
+    public static void main(String[] args) {
+        Examen E1=new Examen();
+        Examen E2 =new Examen(10,"2H10","24/05/2019");
+        E1.Afficher();
+        E2.Afficher();
+    }
+}
