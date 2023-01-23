4787019a365839e0442dbaa77cfb898eb6c4520a
Imad Asserne <imad.asserne@gmail.com>
2023-01-23 19:04:39 +0000

    change at function Calculer_Salaire & Salaire_supplementaire

--- src/Devoir/Enseignant.java
+++ src/Devoir/Enseignant.java
@@ -5,13 +5,24 @@ import java.util.*;
 public class Enseignant extends Personnel{
 private String Grade;
 private int volume_horaire;
+private int nbr_hs=0; // nbr_hs :nombre de heure supplementaire
 private List<Etudiants> ListE =new ArrayList<Etudiants>();
 
 
-    public Enseignant(int code, String nom, String prenom, String grade, int volume_horaire) {
+    public int getNbr_hs() {
+        return nbr_hs;
+    }
+
+    public void setNbr_hs(int nbr_hs) {
+        this.nbr_hs = nbr_hs;
+    }
+
+    public Enseignant(int code, String nom, String prenom, String grade, int volume_horaire, int nbr_hs) {
         super(code, nom, prenom);
         Grade = grade;
         this.volume_horaire = volume_horaire;
+        this.nbr_hs = nbr_hs;
+
     }
 
     public String getGrade() {
@@ -40,13 +51,13 @@ private List<Etudiants> ListE =new ArrayList<Etudiants>();
 
     public void Afficher()
     {
-        System.out.println("les informatons de Enseignant :");
-        System.out.println("Code "+getCode()+" Nom "+getNom()+" Prenom "+getPrenom()+" Grade "+this.Grade+" Nombres de horaire qui travail "+this.volume_horaire);
+        System.out.println("les informatons de Enseignant : "+this.getCode());
+        System.out.println("Code "+getCode()+" Nom "+getNom()+" Prenom "+getPrenom()+" Grade "+this.Grade+" Nombres de horaire qui travail "+this.volume_horaire+"Nombres de horaire pour travail supplementaire "+this.nbr_hs);
     }
 
     @Override
-    public double Calculer_Salaire(int heures, double coefficient) {
-        return 0;
+    public double Calculer_Salaire(double coefficient) {
+        return this.volume_horaire*coefficient;
     }
     public void Ajouter_etudiant( Etudiants e)
     {
@@ -71,9 +82,10 @@ private List<Etudiants> ListE =new ArrayList<Etudiants>();
         double result=0;
         switch(this.Grade)
         {
-            case "PA": result=200*this.volume_horaire;
-            case "PH": result=250*this.volume_horaire;
-            case "PES" :result=300*this.volume_horaire;
+            case "PA": result=200*this.nbr_hs;
+            case "PH": result=250*this.nbr_hs;
+            case "PES" :result=300*this.nbr_hs;
+            default :System.out.print("Les informations que vous avez saisies sont incorrectes  ");
         }
         return result;
     }
