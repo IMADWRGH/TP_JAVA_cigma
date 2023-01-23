05f6f0b607b3056e3861e2aa5befe82398a05417
Imad Asserne <imad.asserne@gmail.com>
2023-01-23 00:44:38 +0000

    Add setter & getter

--- src/Devoir/Personnel.java
+++ src/Devoir/Personnel.java
@@ -5,6 +5,30 @@ public abstract class Personnel {
     private String nom;
     private String prenom;
 
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
     public Personnel(int code, String nom, String prenom) {
         this.code = code;
         this.nom = nom;
