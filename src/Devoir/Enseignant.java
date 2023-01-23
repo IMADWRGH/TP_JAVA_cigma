e339d934e7c01d6ce0092bdf0cd45dd0a9b02fe8
Imad Asserne <imad.asserne@gmail.com>
2023-01-23 01:32:09 +0000

    Add function Add function & Afficher_etd

--- src/Devoir/Enseignant.java
+++ src/Devoir/Enseignant.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Enseignant extends Personnel{
 private String Grade;
 private int volume_horaire;
-private List<Etudiants> ListE =new ArrayList<>();
+private List<Etudiants> ListE =new ArrayList<Etudiants>();
 
 
     public Enseignant(int code, String nom, String prenom, String grade, int volume_horaire) {
@@ -15,7 +15,7 @@ private List<Etudiants> ListE =new ArrayList<>();
     }
     public void Afficher()
     {
-        System.out.println("les informatons de Enseignant");
+        System.out.println("les informatons de Enseignant :");
         System.out.println("Code "+getCode()+" Nom "+getNom()+" Prenom "+getPrenom()+" Grade "+this.Grade+" Nombres de horaire qui travail "+this.volume_horaire);
     }
 
@@ -23,5 +23,21 @@ private List<Etudiants> ListE =new ArrayList<>();
     public double Calculer_Salaire(int heures, double coefficient) {
         return 0;
     }
+    public void Ajouter_etudiant( Etudiants e)
+    {
+        for(int i=0;i<ListE.size() ;i++)
+        {
+            ListE.add(e);
+        }
+    }
+    public void Afficher_etd()
+    {
 
+        for(int i=0;i<ListE.size();i++)
+        {
+            System.out.println("------------");
+            ListE.get(i).Afficher();
+        }
+
+    }
 }
