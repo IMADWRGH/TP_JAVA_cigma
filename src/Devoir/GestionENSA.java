bc3aa11036c164026d0a42d9dcc287b2c5017926
Imad Asserne <imad.asserne@gmail.com>
2023-01-23 00:16:49 +0000

    first commit

---
+++ src/Devoir/GestionENSA.java
@@ -0,0 +1,10 @@
+package Devoir;
+
+
+
+public class GestionENSA {
+    public static void main(String[] args) {
+        Enseignant e =new Enseignant(1,"1","1","PH",250);
+        e.Afficher();
+    }
+}
