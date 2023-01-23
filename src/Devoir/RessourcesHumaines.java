13a3f6ff3bc1066bdb0e4c0032a80c389cf1b583
Imad Asserne <imad.asserne@gmail.com>
2023-01-23 00:43:46 +0000

    Add functions

--- src/Devoir/RessourcesHumaines.java
+++ src/Devoir/RessourcesHumaines.java
@@ -1,8 +1,29 @@
 package Devoir;
 import java.util.*;
 public class RessourcesHumaines {
-List <Enseignant> GRH =new ArrayList<>();
-   // Afficher_Enseignants()
+private int nbr_E;
+private  List <Enseignant> GRH =new ArrayList<>();
+
+    public RessourcesHumaines(int nbr_E) {
+        this.nbr_E = nbr_E;
+    }
+
+    public void Ajouter (Enseignant e)
+{
+    for(int i=0;i<nbr_E-1;i++)
+    {
+        GRH.add(e);
+    }
+
+}
+    public void Afficher_Enseignants()
+{
+    for(int i=0;i<GRH.size();i++)
+    {
+        GRH.get(i).Afficher();
+    }
+
+}
    // Rechercher_Ens( ?)
 
 
