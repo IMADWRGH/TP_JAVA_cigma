20dea8c625e0910bdc448804b101ea7dd4d1a9d9
Imad Asserne <imad.asserne@gmail.com>
2023-01-23 00:45:13 +0000

    just test

--- src/Devoir/GestionENSA.java
+++ src/Devoir/GestionENSA.java
@@ -4,7 +4,11 @@ package Devoir;
 
 public class GestionENSA {
     public static void main(String[] args) {
-        Enseignant e =new Enseignant(1,"1","1","PH",250);
-        e.Afficher();
+        Enseignant rr =new Enseignant(1,"1","1","PH",250);
+        Enseignant E =new Enseignant(2,"2","2","PH",300);
+        RessourcesHumaines r =new RessourcesHumaines(2);
+ r.Ajouter(rr);
+ r.Ajouter(E);
+ r.Afficher_Enseignants();
     }
 }
