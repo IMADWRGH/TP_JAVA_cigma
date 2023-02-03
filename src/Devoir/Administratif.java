34f383b954f72cf60642fa96ef551d1b17c1573b
Imad Asserne <imad.asserne@gmail.com>
2023-02-03 20:40:30 +0000

    add function Afficher

--- src/Devoir/Administratif.java
+++ src/Devoir/Administratif.java
@@ -2,12 +2,17 @@ package Devoir;
 
 public class Administratif extends Personnel{
 
-    public Administratif(int code, String nom, String prenom) {
-        super(code, nom, prenom);
+
+    public Administratif(int code, String nom, String prenom, int bureau, double salaire, double prime) {
+        super(code, nom, prenom, bureau, salaire, prime);
     }
 
     @Override
     public double Calculer_Salaire(double coefficient) {
-        return 0;
+        return coefficient* super.getSalaire();
+    }
+    public void Afficher(){
+        System.out.println("les informatons de Administratif : "+super.getCode());
+        System.out.println("Code "+getCode()+" Nom "+getNom()+" Prenom "+getPrenom()+"Bureau "+getBureau()+"Salaire "+getSalaire()+"Prime "+getPrime());
     }
 }
