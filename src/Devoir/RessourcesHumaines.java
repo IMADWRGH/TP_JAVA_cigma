25bd429e79d609fb6b7d9c24fd35f1733f8fa85d
Imad Asserne <imad.asserne@gmail.com>
2023-02-05 23:13:37 +0000

    add f_Rechercher_Ens

--- src/Devoir/RessourcesHumaines.java
+++ src/Devoir/RessourcesHumaines.java
@@ -2,10 +2,11 @@ package Devoir;
 import java.util.*;
 public class RessourcesHumaines {
 private int nbr_E;
-private  List <Enseignant> GRH =new ArrayList<>();
+private  List <Enseignant> GRH;
 
     public RessourcesHumaines(int nbr_E) {
         this.nbr_E = nbr_E;
+        this.GRH= new ArrayList<>();
     }
 
     public void Afficher_Enseignants()
@@ -16,11 +17,20 @@ private  List <Enseignant> GRH =new ArrayList<>();
     }
 
 }
-   public int  Rechercher_Ens(Enseignant e )
-   {
-               return GRH.indexOf(e);
-
-   }
+//   public int  Rechercher_Ens(int code )
+//   {
+//
+//       for (Enseignant e :GRH)
+//           if (e.getCode() == code)
+//               return GRH.indexOf(e);
+//   }
+    public int Rechercher_Ens(int code ) {
+        int result = 0;
+        for (Enseignant e : GRH)
+            if (e.getCode()==code)
+                return result=GRH.indexOf(e);
+       return result;
+    }
 
   public void Ajouter_groupe(Enseignant e)
   {
