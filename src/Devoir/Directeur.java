e0abb556194e59497b2ec39a560da8c577ee46fb
Imad Asserne <imad.asserne@gmail.com>
2023-02-05 17:33:00 +0000

    make Directeur a class singleton

--- src/Devoir/Directeur.java
+++ src/Devoir/Directeur.java
@@ -1,12 +1,22 @@
 package Devoir;
 
 public class Directeur extends Personnel{
-  private Directeur(int code, String nom, String prenom) {
-        super(code, nom, prenom);
+    private static Directeur directeur;
+    private Directeur(int code, String nom, String prenom, int bureau, double salaire,double prime) {
+        super(code, nom, prenom,bureau,salaire,prime);
     }
 
+    public static Directeur getInstance(int code, String nom, String prenom, int bureau, double salaire,double prime){
+        if (directeur==null)
+        {
+            directeur=new Directeur(code, nom, prenom,bureau,salaire,prime);
+        }else {
+             System.out.println("C'est déjà créé impossible de recréer");
+        }
+        return directeur;
+    }
     @Override
-    public double Calculer_Salaire(double coefficient) {
-        return 0;
+    public double Calculer_Salaire() {
+        return getSalaire()+getPrime();
     }
 }
