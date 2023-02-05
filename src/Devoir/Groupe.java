52b40fa07d2d93be30c6b674d2a0ef2eec8dac30
Imad Asserne <imad.asserne@gmail.com>
2023-02-05 16:07:09 +0000

    change attribut,getter,setter,f_Afficher &

--- src/Devoir/Groupe.java
+++ src/Devoir/Groupe.java
@@ -5,12 +5,42 @@ import java.util.*;
 public class Groupe {
 
     private String nomGroupe;
-    private List<Etudiants> ListEtudiant=new ArrayList<>();
-    private Map<String, List<Etudiants>> MapE=new HashMap<>();
+    private List<Etudiants> ListEtudiant;
 
-    public Groupe(String nomGroupe, List<Etudiants> listEtudiant, Map<String, List<Etudiants>> mapE) {
+    public Groupe(String nomGroupe) {
         this.nomGroupe = nomGroupe;
+        ListEtudiant =new ArrayList<>();
+    }
+
+    public String getNomGroupe() {
+        return nomGroupe;
+    }
+
+    public void setNomGroupe(String nomGroupe) {
+        this.nomGroupe = nomGroupe;
+    }
+
+    public List<Etudiants> getListEtudiant() {
+        return ListEtudiant;
+    }
+
+    public void setListEtudiant(List<Etudiants> listEtudiant) {
         ListEtudiant = listEtudiant;
-        MapE = mapE;
+    }
+
+    public void Ajouter(Etudiants e){
+        ListEtudiant.add(e);
+    }
+    public void Afficher(int code){
+        for (Etudiants e: ListEtudiant){
+            if (e.getCode()==code){
+                e.Afficher();
+            }
+        }
+    }
+    public void Afficher(){
+        System.out.println("les étudiants de groupe : "+this.nomGroupe);
+        for (Etudiants e :ListEtudiant)
+            e.Afficher();
     }
 }
